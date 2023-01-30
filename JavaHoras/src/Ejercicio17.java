e93b641a8dafd1035670e14f7db9311341c6fd79
inoricode28 <inori237@gmail.com>
2023-01-30 01:56:41 -0500

    Ejercicio18

---
+++ JavaHoras/src/Ejercicio17.java
@@ -0,0 +1,11 @@
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        String[] carro = new String[3];
+        carro[0] = "Ferrari";
+        carro[1] = "Mercedes";
+        carro[2] = "Audi";
+        for (int i = 0; i < carro.length; i++) {
+            System.out.println(carro[i]);
+        }
+    }
+}
---
+++ JavaHoras/src/Ejercicio18.java
@@ -0,0 +1,9 @@
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        String[] carro ={"Camaro","Convertible","Tesla"};
+
+        carro[0] = "Honda"; //Esta sentencia no se puede modificar y reemplaza al indice 0
+
+        System.out.println(carro[0]);
+    }
+}
