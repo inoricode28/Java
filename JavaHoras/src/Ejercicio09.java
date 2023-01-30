25c5ac3e8bf9136f9f580b5307ac625f5a5a0c42
inoricode28 <inori237@gmail.com>
2023-01-29 19:39:28 -0500

    ejer09

--- JavaHoras/src/Ejercicio09.java
+++ JavaHoras/src/Ejercicio09.java
@@ -1,5 +1,18 @@
 public class Ejercicio09 {
     public static void main(String[] args) {
-        
+        int edad = 75;
+        if (edad == 75) {
+            System.out.println("Ok Boomer!");
+        }
+        else if (edad >= 18) {
+            System.out.println("Eres un Adulto");
+        }
+        else if(edad>=13){
+            System.out.println("Eres un Adolescente!");
+        }
+        else{
+            System.out.println("Tu no eres un adulto!");
+        }
+
     }
 }
