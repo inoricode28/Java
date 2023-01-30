fe8f0944514a6afd6dc2ec6ce4a66eef2463ecfd
inoricode28 <inori237@gmail.com>
2023-01-30 01:33:05 -0500

    Ejercicio14

--- JavaHoras/src/Ejercicio13.java
+++ JavaHoras/src/Ejercicio13.java
@@ -1,6 +1,7 @@
 public class Ejercicio13 {
     public static void main(String[] args) {
-        while(1==1){
+        int a=1, b=1;
+        while(a==b){
             System.out.println("Hola");
         }
     }
--- JavaHoras/src/Ejercicio14.java
+++ JavaHoras/src/Ejercicio14.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Ejercicio14 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nombre ="";
+
+        do{
+            System.out.println("Ingrese su nombre: ");
+            nombre = scanner.nextLine();
+        }while(nombre.isBlank());
+        System.out.println("Hola " + nombre);
+        scanner.close();    
+
         
     }
 }
