64c44c2504ec75afb18da3106cff50fe020ab380
inoricode28 <inori237@gmail.com>
2023-01-29 19:48:38 -0500

    Ejercicio10

---
+++ JavaHoras/src/Ejercicio10.java
@@ -0,0 +1,15 @@
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        String dia = "Martes";
+        switch (dia) {
+            case "Domingo": System.out.println("Hoy es Domingo"); break;
+            case "Lunes": System.out.println("Hoy es Lunes"); break;
+            case "Martes": System.out.println("Hoy es Martes"); break;
+            case "Miercoles": System.out.println("Hoy es Miercoles");break;
+            case "Jueves": System.out.println("Hoy es Jueves");break;
+            case "Viernes": System.out.println("Hoy es Viernes");break;
+            case "Sabado": System.out.println("Hoy es Sabado");break;
+        }
+    }
+
+}
