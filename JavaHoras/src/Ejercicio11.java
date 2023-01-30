5d30120bfae372cba4dfa491effb5454d18f7b06
inoricode28 <inori237@gmail.com>
2023-01-29 20:00:49 -0500

    Ejercicio11

---
+++ JavaHoras/src/Ejercicio11.java
@@ -0,0 +1,15 @@
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        int temp =25;
+        if(temp>30){
+            System.out.println("Hace calor afuera");
+        }
+        else if(temp>=20 && temp<=30){
+
+            System.out.println("Es calido afuera");
+        }
+        else{
+            System.out.println("Esta frio afuera");
+    }
+ }
+}
